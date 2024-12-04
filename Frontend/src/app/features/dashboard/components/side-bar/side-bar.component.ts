5416e1a019efefd2f1f0eeebbfab8d1869924b2d
RANDEVTEAM\RandevTeam <tannertahirykevin@gmail.com>
2024-12-04 17:14:32 +0300

    sidebarlest: fix dynamique style with computed adn signal

--- Frontend/src/app/features/dashboard/components/side-bar/side-bar.component.ts
+++ Frontend/src/app/features/dashboard/components/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { sideLeftBarState } from '../../store/signal.store';
@@ -7,17 +7,55 @@ import { sideLeftBarState } from '../../store/signal.store';
   selector: 'app-side-bar',
   imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss',
+  styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent {
   private readonly router = inject(Router);
 
   sideLeftBarState = sideLeftBarState;
-  
+  sideRightbarState = signal(false);
+
+  readonly classes = computed(() => {
+    const isOpened = this.sideRightbarState();
+
+    return {
+      listClass: isOpened ? 'rounded-7' : 'text-white',
+      iconClass: isOpened ? 'text text-gray-800' : 'text-white',
+      textClass: isOpened ? 'font-semibold text-gray-800' : 'font-semibold text-white',
+      arrowClass: isOpened ? 'rotate-90 text text-gray-800' : 'text-white',
+
+      menuIconClass: (route: string) => this.isActive(route) ? 'menu-icon-solid' : 'menu-icon-outline',
+      textClassList: (route: string) => this.isActive(route) ? 'text' : 'text-white',
+      arrowClassDropDown: isOpened ? 'rotate-90 text' : 'text-white',
+    };
+  });
+  readonly dropdownClasses = computed(() => {
+    const isOpened = this.sideRightbarState();
+    return {
+      listClasses: {
+        'rounded-7': isOpened,
+        'text-white': !isOpened,
+      },
+      iconClasses: {
+        text: isOpened,
+        'text-white': !isOpened,
+      },
+      textClasses: {
+        text: isOpened,
+        'text-white': !isOpened,
+      },
+      arrowClasses: {
+        'rotate-90': isOpened,
+        text: isOpened,
+        'text-white': !isOpened,
+      },
+    };
+  });
+  toggleDropdown() {
+    this.sideRightbarState.set(!this.sideRightbarState());
+  }
+
   isActive(route: string): boolean {
     return this.router.url.includes(route);
   }
-
 }
-
-
