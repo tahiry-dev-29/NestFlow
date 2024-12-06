7fb7b4e70302d94405db58675a9ca304991f4acf
RANDEVTEAM\RandevTeam <tannertahirykevin@gmail.com>
2024-12-06 17:39:17 +0300

    #

--- Frontend/src/app/features/dashboard/components/dashboard-overview/dashboard-overview.component.ts
+++ Frontend/src/app/features/dashboard/components/dashboard-overview/dashboard-overview.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../side-bar/side-bar.component';
 import { HeaderComponent } from '../header/header.component';
-import { SideBarRightComponent } from '../../../shared/components/side-bar-right/side-bar-right.component';
-import {  sideLeftBarState, sideRightbarState } from '../../store/signal.store';
+import { sideLeftBarState, sideRightbarState } from '../../store/signal.store';
+
 @Component({
   selector: 'app-dashboard-overview',
   imports: [
     RouterOutlet,
     SideBarComponent,
-    HeaderComponent,
-    SideBarRightComponent,
-  ],
+    HeaderComponent
+    ],
   templateUrl: './dashboard-overview.component.html',
-  styleUrl: './dashboard-overview.component.scss',
+  styleUrls: ['./dashboard-overview.component.scss'],
 })
 export class DashboardOverviewComponent {
   sideLeftBarState = sideLeftBarState;
-  sidebarState = sideRightbarState;
-  toggleRight() {
-    this.sidebarState.update((state) => !state);
+  // sidebarState = sideRightbarState;
+  isSmallScreen = false;
+
+  constructor() {
+    this.checkScreenSize(); 
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    this.isSmallScreen = window.innerWidth < 1200;
+    if (this.isSmallScreen) {
+      this.sideLeftBarState.update(() => false);
+    }
+  }
+
+  // toggleRight() {
+  //   this.sidebarState.update((state) => !state);
+  // }
 }
