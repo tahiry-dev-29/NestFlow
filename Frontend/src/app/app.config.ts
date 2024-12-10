33fe262a18a34920b27d6004cb3531bb1d6fdd9f
RANDEVTEAM\RandevTeam <tannertahirykevin@gmail.com>
2024-12-10 17:02:35 +0300

    core fix and add ngrx signal and ngrx store

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-  ],
+    provideStore()
+],
 };
