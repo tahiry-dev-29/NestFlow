3be2c8f751d78550cd24bfea1bda46e2da61ed22
RANDEVTEAM\RandevTeam <tannertahirykevin@gmail.com>
2024-12-12 14:22:04 +0300

    remove items

--- Frontend/src/app/features/dashboard/components/side-bar/sidebar-menu-item/menu-item.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface MenuItem {
-  label: string;
-  route: string;
-  icon: string;
-  dropdown: boolean;
-  style?: 'settings' | 'primary'; // Types limités pour "style"
-  children?: { label: string; route: string }[]; // Enfants (facultatifs)
-}
