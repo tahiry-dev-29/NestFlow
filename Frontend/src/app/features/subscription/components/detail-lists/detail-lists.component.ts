d7cbd34059e92658ed9d43dfeb994d73100c96ab
RANDEVTEAM\RandevTeam <tannertahirykevin@gmail.com>
2024-12-17 13:48:55 +0300

    fix  code clean

--- Frontend/src/app/features/subscription/components/detail-lists/detail-lists.component.ts
+++ Frontend/src/app/features/subscription/components/detail-lists/detail-lists.component.ts
@@ -3,7 +3,6 @@ import { Component, inject, input, InputSignal, signal } from '@angular/core';
 import { ToastrModule, ToastrService } from "ngx-toastr";
 import { expandCollapse } from '../../../shared/animations/animations';
 import { PopupsComponent } from "../../../shared/components/popups/popups.component";
-import { FilterSubscribersPipe } from '../../../shared/pipe/filter-search.pipe';
 import { SubscriptionStore } from '../../store/subscribed.store';
 import { Router } from '@angular/router';
 import { ISubscription } from '../../models/subscription.interface';
@@ -11,13 +10,13 @@ import { ISubscription } from '../../models/subscription.interface';
 @Component({
   selector: 'app-detail-lists',
   standalone: true,
-  imports: [CommonModule, FilterSubscribersPipe, PopupsComponent, ToastrModule],
+  imports: [CommonModule, PopupsComponent, ToastrModule],
   templateUrl: './detail-lists.component.html',
   styleUrl: './detail-lists.component.scss',
   animations: [expandCollapse],
 })
 export class DetailListsComponent {
-  private store = inject(SubscriptionStore);
+  store = inject(SubscriptionStore);
   private toastr = inject(ToastrService);
   private router = inject(Router);
 
@@ -26,24 +25,14 @@ export class DetailListsComponent {
     search: '',
   });
 
-  subscrib = this.store.subscriptions;
-  expandedId = this.store.expandedId;
-  expandedMenuId = this.store.expandedMenuId;
   showPopup = signal(false);
   subscriberToDelete = signal<number | null>(null);
   showAddForm = signal(false);
 
-  getProgressClasses(progress: number): string {
-    if (progress === 100) return 'bg-green-500';
-    if (progress >= 75) return 'bg-blue-500';
-    if (progress >= 50) return 'bg-yellow-500';
-    return 'bg-red-500';
-  }
-
-
   getRemainingDays(abonneId: number) {
-    const subscription = this.store.subscriptions()?.find(s => s.id === abonneId);
-    return subscription ? this.calculateRemainingDays(subscription) : 0;
+    const subscription = this.store.getSubscriptionById(abonneId);
+    if (!subscription) return 0; // Handle null case
+    return this.calculateRemainingDays(subscription);
   }
 
   private calculateRemainingDays(subscription: ISubscription): number {
@@ -51,20 +40,6 @@ export class DetailListsComponent {
     const today = new Date();
     const differenceInTime = endDate.getTime() - today.getTime();
     return Math.ceil(differenceInTime / (1000 * 3600 * 24));
-  }
-
-  getRemainingProgress(id: number): number {
-    const subscription = this.store.subscriptions()?.find((s) => s.id === id);
-    return subscription?.progress ?? 0;
-  }
-
-
-  toggleDetails(id: number) {
-    this.store.toggleDetails(id);
-  }
-
-  toggleMenu(id: number) {
-    this.store.toggleMenu(id);
   }
 
   toggleAddForm() {
@@ -83,7 +58,7 @@ export class DetailListsComponent {
 
   confirmDelete() {
     const id = this.subscriberToDelete();
-    const fullname = this.subscrib().find(abonne => abonne.id === id)?.fullname ?? '';
+    const fullname = this.store.getSubscriptionById(id!)?.fullname ?? '';
     if (id !== null) {
       this.store.deleteSubscription(id);
       this.toastr.success(
